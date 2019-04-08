cb8402afe1128867424fdd87cce1ebedd60041ac
ElSomberero2000 <jack.brogan@outlook.com>
2019-04-08 01:49:47 +0100

    Imported sourced audio, added scene persistent background music

---
+++ Assets/MyContent/Scripts/AudioFix.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioFix : MonoBehaviour
+{
+    static AudioFix instance = null;
+
+    private void Awake()
+    {
+        // Allows ongoing audio to continue after loading a new scene
+        DontDestroyOnLoad(transform.gameObject);
+
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
